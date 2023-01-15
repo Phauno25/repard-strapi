31fe825de158d20f5aad8dfda3ea82206a940b95
Pablo C <Sho.Pablo.25@hotmail.com>
2023-01-15 02:33:23 -0300

    plugin

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS',["4R3yGwmInnJsCPGPKqf/Cw==","Yt6XTD/3eUJ3D3t1Pt67VA==","TqHuzj3Clg1NnyAicGQHFQ==","jTQKuMGEchYV52evLmlNwA=="]),
+    keys: env.array('APP_KEYS'),
   },
 });
