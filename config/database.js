8668b6d6ef40730d77a72903c44682e83ba195f1
Pablo C <Sho.Pablo.25@hotmail.com>
2023-01-15 01:35:32 -0300

    commit a mi repo

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'db.qpugbbffinpgedsstjnk.supabase.co'),
+      host: env('DATABASE_HOST', 'db.etmgkqrhwzzdsorlktce.supabase.co'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'iG9Wy80HX25gJxRG'),
+      password: env('DATABASE_PASSWORD', 'pablokevinrepard*'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
